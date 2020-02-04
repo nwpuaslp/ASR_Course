5d82fb15826b1b8016f833877aaf6139bde6ba20
zhuoyuan yao <zhyyao@nwpu-aslp.org>
2020-02-04 19:12:39 +0800

    remove 05

--- 05-/lab/calc-wer.sh
+++
@@ -1,7 +0,0 @@
-ref=$1
-reco=$2
-
-python convert_fmt.py $ref ${ref}.fmt
-python convert_fmt.py $reco ${reco}.fmt
-
-python compute-wer.pyc --v=1 ${ref}.fmt ${reco}.fmt
--- 05-/lab/draw_fsm.sh
+++
@@ -1,3 +0,0 @@
-# if you install openfst and dot, you could visualize the fsm in pdf
-# some fsm file contains more than one fsm, you need to split it by hand before using this cmd.
-grep -v '#' p018k1.noloop.fsm|fstcompile --osymbols=p018k2.syms|fstdraw -portrait --osymbols=p018k2.syms|dot -Tpdf -o p018k1.noloop.pdf
